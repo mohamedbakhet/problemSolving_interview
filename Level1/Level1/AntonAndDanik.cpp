b241f2b9c13004bf766dc0690787474b332ebeb0
mohamedbakhet <mohamed33bakhet@gmail.com>
2022-04-05 23:24:09 +0200

    solve 3 problem in codeforces

---
+++ Level1/Level1/AntonAndDanik.cpp
@@ -0,0 +1,28 @@
+/*
+ #include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int sumA = 0;
+	int sumD = 0;
+
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++)
+	{
+		
+		if (s[i] == 'A')
+			sumA++;
+		else if(s[i] == 'D')
+			sumD ++;
+	}
+	if (sumA > sumD)
+		cout << "Anton";
+	else if (sumA < sumD)
+		cout << "Danik";
+	else
+		cout << "Friendship";
+	return 0;
+}
+*/
---
+++ Level1/Level1/VanyaAndFence.cpp
@@ -0,0 +1,20 @@
+/*
+#include<iostream>
+using namespace std;
+int main() {
+	int n, h;
+	cin >> n >> h;
+	int sum=0;
+	int t;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> t;
+		if (t <= h)
+			sum++;
+		else
+			sum += 2;
+	}
+	cout << sum;
+	return 0;
+}
+*/
